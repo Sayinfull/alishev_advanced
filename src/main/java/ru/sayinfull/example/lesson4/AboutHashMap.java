a14dd6da24ba20a9d3c2b17ae53401605afcdb76
Sayinfull <magen-96@mail.ru>
2022-07-18 16:03:41 +0300

    HashMap содержит в себе два значения, которые можно отображать по разному. Напрмер, Integer и String. У этого класса нет определенного порядка и ключи вызываются хаотично. Если нужно сохранить порядок, то его лучше не использовать. Также есть несколько методов: Entry - которые вызывает одну ключ-пару entrySet - который вызывает все ключи getKey  - вызывает ключ и getValue - второе значение пары-ключа

---
+++ src/main/java/ru/sayinfull/example/lesson4/AboutHashMap.java
@@ -0,0 +1,28 @@
+package ru.sayinfull.example.lesson4;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AboutHashMap {
+    public static void main(String[] args) {
+        Map<Integer, String> map = new HashMap<>();
+
+        map.put(1, "one");
+        map.put(2, "two");
+        map.put(3, "three");
+
+        System.out.println(map);
+
+        map.put(3,  "other value for key"); // когда мы вставляем дубликат значения, то оно заменяется на новое.
+        System.out.println(map);
+
+        System.out.println(map.get(1));
+
+        //Map.Entry вызывает пару ключ-значение ( т.е. 1, "one") и т.д.
+        // map.entrySet вызывает все ключи, которые есть в мэпе
+
+        for (Map.Entry<Integer, String> entry: map.entrySet()){
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        }
+    }
+}
