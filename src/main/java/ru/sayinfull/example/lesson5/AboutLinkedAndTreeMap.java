db35e7df5f735a572a08fd0249f9b4589069ffc9
Sayinfull <magen-96@mail.ru>
2022-07-18 17:35:07 +0300

    HashMap - сортирует по ключу рандомно все значения. LinkedMap - выводит на экран пары в том порядке, в каком они были записаны. TreeMap - сортирует пары по ключу в естественном порядке.

---
+++ src/main/java/ru/sayinfull/example/lesson5/AboutLinkedAndTreeMap.java
@@ -0,0 +1,29 @@
+package ru.sayinfull.example.lesson5;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class AboutLinkedAndTreeMap {
+    public static void main(String[] args) {
+        Map<Integer,String> hashMap = new HashMap<>();// нет порядка
+        Map<Integer, String> linkedMap = new LinkedHashMap<>(); // есть порядок(сортировка) в том порядке
+        // в каком их добавили
+        Map<Integer,String> treeMap = new TreeMap<>(); // все данные отсортированы по ключу значения (от 0 до 10 например)
+            testMap(treeMap);
+    }
+
+    public static void testMap(Map<Integer, String> map){
+        map.put(39,"Bob");
+        map.put(12, "Chris");
+        map.put(77, "Tom");
+        map.put(0, "Tim");
+        map.put(7, "Sos");
+        map.put(1500, "Rose");
+
+        for (Map.Entry<Integer,String> entry : map.entrySet()){
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        }
+    }
+}
