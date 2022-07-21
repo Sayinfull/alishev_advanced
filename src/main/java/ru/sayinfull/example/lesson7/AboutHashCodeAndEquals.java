a73dd64e1f391ba600f03da601970dadd464a63a
Sayinfull <magen-96@mail.ru>
2022-07-21 12:43:26 +0300

    HashCode and Equals. Если создавать два объекта класса и с помощью одинаковых методов конструктора вложить данные, то они могут повторяться и тогда метода map and set будут работать неправильно. Для этого стоит использовать hashcode and equals, чтобы методы работали правильно и они ссылались на объекты класса (их память) и сравнивала данные внутри объектов.

---
+++ src/main/java/ru/sayinfull/example/lesson7/AboutHashCodeAndEquals.java
@@ -0,0 +1,54 @@
+package ru.sayinfull.example.lesson7;
+
+import java.util.*;
+
+public class AboutHashCodeAndEquals {
+    public static void main(String[] args) {
+        Map<Person, String> map = new HashMap<>();
+        Set<Person> set = new HashSet<>();
+
+        Person person1 = new Person(1, "Salikh");
+        Person person2 = new Person(1, "Salikh");
+
+        map.put(person1, "123");
+        map.put(person2, "234");
+
+        set.add(person1);
+        set.add(person2);
+
+        System.out.println(set);
+        System.out.println(map);
+    }
+}
+
+class Person {
+    private int id;
+    private String name;
+
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public Person(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return id == person.id && Objects.equals(name, person.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+}
