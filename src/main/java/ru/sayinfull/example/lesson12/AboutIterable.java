72b9bb8cd73600943e6cd1110de9fa79e34db263
Sayinfull <magen-96@mail.ru>
2022-07-25 10:43:02 +0300

    Iterable. Является основой для for each. Необходим в том случае, если, напрмер, нужно удалить во время перебирания списка какой либо элемент.

---
+++ src/main/java/ru/sayinfull/example/lesson12/AboutIterable.java
@@ -0,0 +1,30 @@
+package ru.sayinfull.example.lesson12;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class AboutIterable {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        Iterator<Integer> iterator = list.iterator();
+
+        int index = 0;
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+            if (index == 1) {
+                iterator.remove();
+                index++;
+            }
+            System.out.println(list);
+        }
+
+//        for (int x : list) {
+//            System.out.println(x);
+//        }
+    }
+}
