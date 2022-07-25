0876c8845b3fdff4978248d6eba4ece4ec940f90
Sayinfull <magen-96@mail.ru>
2022-07-25 10:16:09 +0300

    Stack. Интерфейс полностью противоположен Queue, в котором извлекаются элементы из списка с конца.

---
+++ src/main/java/ru/sayinfull/example/lesson11/AboutStack.java
@@ -0,0 +1,21 @@
+package ru.sayinfull.example.lesson11;
+
+import java.util.Stack;
+
+public class AboutStack {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+
+        stack.push(1); // метод Push добавляет элемент в список
+        stack.push(10);
+        stack.push(7);
+
+        while (!stack.empty()){
+            System.out.println(stack.pop());
+        }
+
+        System.out.println(stack.pop()); // извлекает последний элемент из списка
+        System.out.println(stack.pop());
+
+    }
+}
